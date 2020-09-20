098222dcea13027cec948ea51453a18d610e7a5e
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-20 03:45:37 +0000

    Automatically backed up by Learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,16 +4,16 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-    <h1>Actors Page</h1>
-    // {actors.map((e) => (
-    //     <div>
-    //         <h3>Name: {e.name}</h3>
-    //         <p>Movies:</p>
-    //         <ul>
-    //             {actor.movies.map((e) => <li >{e}</li> )}
-    //         </ul>
-    //     </div>
-    // ))}
+      <h1>Actors Page</h1>
+      {actors.map((e) => (
+          <div>
+              <h3>Name: {e.name}</h3>
+              <p>Movies:</p>
+              <ul>
+                  {actor.movies.map((e) => <li >{e}</li> )}
+              </ul>
+          </div>
+      ))}
     </div>
   );
 };
